f4e217d266d56477cc3aa6a07fb9e66e690e8608
4attye <167115532+4attye@users.noreply.github.com>
2025-06-18 22:19:05 +0300

    HW

--- HW_1.py
+++ HW_1.py
@@ -1,3 +1,6 @@
+import timeit
+
+
 def find_coins_greedy(amount, banknotes):
     
     banknotes = sorted(banknotes, reverse=True)  
@@ -12,6 +15,38 @@ def find_coins_greedy(amount, banknotes):
 
     return result
 
+
+def find_min_coins(amount, banknotes):
+    # Ініціалізуєм таблиці dp
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0 
+
+    # prev зберігатиме останню використану монету для побудови суми
+    prev = [None] * (amount + 1)
+
+    # Основний цикл для заповнення dp
+    for bill in banknotes:
+        for i in range(bill, amount + 1):
+            if dp[i - bill] + 1 < dp[i]:
+                dp[i] = dp[i - bill] + 1
+                prev[i] = bill
+
+    # Якщо сума не може бути досягнута, повертаємо порожній словник
+    if dp[amount] == float('inf'):
+        return {}
+
+    # Відновлеєм результат
+    result = {}
+    current = amount
+    while current > 0:
+        bill = prev[current]
+        if bill is None:
+            break
+        result[bill] = result.get(bill, 0) + 1
+        current -= bill
+
+    return result
+
 if __name__ == "__main__":
    
     target_sum = 113 
@@ -19,3 +54,19 @@ if __name__ == "__main__":
 
     res = find_coins_greedy(target_sum, banknotes)
     print(res)
+
+    res = find_min_coins(target_sum, banknotes)
+    print(res)
+
+    # setup = f"from __main__ import find_coins_greedy, find_min_coins, target_sum, banknotes"
+
+    # t_find_coins_greedy = timeit.timeit("find_coins_greedy(target_sum, banknotes.copy())"
+    #                                     , setup=setup
+    #                                     , number=1)
+    
+    # t_find_min_coins = timeit.timeit("find_min_coins(target_sum, banknotes.copy())"
+    #                                  , setup=setup
+    #                                  , number=1)
+
+    # print(f"Time for greedy algorithm: {t_find_coins_greedy:.6f} seconds")
+    # print(f"Time for dynamic programming algorithm: {t_find_min_coins:.6f} seconds")
